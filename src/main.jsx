1ae6cdcb480792f4c55a339e974466f196dd0747
francilenesalustiano4 <francilenesalustiano4@gmail.com>
2026-01-06 14:59:39 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./App.jsx";
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
